62e2cafe87b0a95e7d1b405cc35c4711a5a59333
Felix Köpge <felix.koepge@schmidt-embedded-systems.de>
2022-04-16 15:56:49 +0200

    refactor calculateValueCountDelta() and calculateSetChange() from DummyMain into Utility class

--- src/main/java/de/ddm/DummyMain.java
+++ src/main/java/de/ddm/DummyMain.java
@@ -5,84 +5,67 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class DummyMain {
-        static Source createSource() {
-                try {
-                        Table table = Table.parseCSV(
-                                        "$,Origin,Destination,Day\n" +
-                                                        "0,Mercury,Venus,2\n" +
-                                                        "1,Mercury,Venus,2\n" +
-                                                        "2,Venus,Earth,3\n" +
-                                                        "3,Earth,Earth,4\n" +
-                                                        "4,Moon,Earth,2\n" +
-                                                        "5,Mars,Moon,1",
-                                        "test.csv");
+    static Source createSource() {
+        try {
+            Table table = Table.parseCSV(
+                    "$,Origin,Destination,Day\n" +
+                    "0,Mercury,Venus,2\n" +
+                    "1,Mercury,Venus,2\n" +
+                    "2,Venus,Earth,3\n" +
+                    "3,Earth,Earth,4\n" +
+                    "4,Moon,Earth,2\n" +
+                    "5,Mars,Moon,1",
+                    "test.csv");
 
-                        Source source = new DummySource(new ArrayList<>(Arrays.asList(table)));
-                        return source;
-                } catch (Exception e) {
-                        assert (false);
-                }
-                return null;
+            Source source = new DummySource(new ArrayList<>(Arrays.asList(table)));
+            return source;
+        } catch (Exception e) {
+            assert (false);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Source source = createSource();
+        InputSplitter inputSplitter = new InputSplitter(source, 1);
+
+        Map<String, ColumnArray> columnArrays = new HashMap<>();
+        Map<String, ColumnSet> columnSets = new HashMap<>();
+        CandidateGenerator generator = new NaiveCandidateGenerator();
+
+        Table nextTable = source.nextTable();
+        while (nextTable != null) {
+            System.out.println("run mainloop");
+
+            /* initialize column-array and column-set if necessary */
+            for (String attribute : nextTable.attributes) {
+                columnArrays.putIfAbsent(attribute, new HeapColumnArray());
+                columnSets.putIfAbsent(attribute, new HeapColumnSet());
+            }
+
+            List<SetCommand> setCommands = inputSplitter.splitTable(nextTable);
+
+            /* process set commands */
+            for (SetCommand setCommand : setCommands) {
+                List<Value> oldValues = columnArrays.get(setCommand.attribute)
+                        .setValue(setCommand.values);
+
+                Map<Value, Long> deltaCounts = Utility.calculateValueDeltaCounts(setCommand.values, oldValues);
+
+                Map<Value, Long> totalCounts = columnSets.get(setCommand.attribute)
+                        .applyCounts(deltaCounts);
+
+                SetChange change = Utility.calculateSetChange(addedValues, removedValues);
+
+                generator.addChange(setCommand.attribute, change);
+            }
+            nextTable = source.nextTable();
         }
 
-        public static void main(String[] args) {
-                Source source = createSource();
-                InputSplitter inputSplitter = new InputSplitter(source, 1);
+        Set<Candidate> candidates = generator.generateCandidates();
 
-                Map<String, ColumnArray> columnArrays = new HashMap<>();
-                Map<String, ColumnSet> columnSets = new HashMap<>();
-                CandidateGenerator generator = new NaiveCandidateGenerator();
+        System.out.println("# Candidates");
+        candidates.forEach(cand -> System.out.println(cand));
 
-                Table nextTable = source.nextTable();
-                while (nextTable != null) {
-                        System.out.println("run mainloop");
-
-                        /* initialize column-array and column-set if necessary */
-                        for (String attribute : nextTable.attributes) {
-                                columnArrays.putIfAbsent(attribute, new HeapColumnArray());
-                                columnSets.putIfAbsent(attribute, new HeapColumnSet());
-                        }
-
-                        List<SetCommand> setCommands = inputSplitter.splitTable(nextTable);
-
-                        /* process set commands */
-                        for (SetCommand setCommand : setCommands) {
-                                List<Value> changedValues = columnArrays.get(setCommand.attribute)
-                                                .setValue(setCommand.values);
-
-                                /* calculate count delta */
-                                Map<Value, Long> decreasedCounts = changedValues.stream().collect(Collectors.groupingBy(
-                                                v -> v, Collectors.counting()));
-                                Map<Value, Long> deltaCounts = setCommand.values.stream().collect(Collectors.groupingBy(
-                                                v -> v.value, Collectors.counting()));
-                                for (Map.Entry<Value, Long> entry : decreasedCounts.entrySet()) {
-                                        deltaCounts.merge(entry.getKey(), -entry.getValue(), (positiveCount,
-                                                        negativeCount) -> positiveCount - negativeCount);
-                                        // TODO remove if delta count is 0
-                                }
-
-                                Map<Value, Long> totalCounts = columnSets.get(setCommand.attribute)
-                                                .applyCounts(deltaCounts);
-
-                                Set<Value> addedValues = totalCounts.entrySet().stream()
-                                                .filter(entry -> entry.getValue() == deltaCounts.get(entry.getKey()))
-                                                .map(Map.Entry::getKey)
-                                                .collect(Collectors.toSet());
-                                Set<Value> removedValues = totalCounts.entrySet().stream()
-                                                .filter(entry -> entry.getValue() == 0)
-                                                .map(Map.Entry::getKey)
-                                                .collect(Collectors.toSet());
-                                SetChange change = new SetChange(addedValues, removedValues);
-
-                                generator.addChange(setCommand.attribute, change);
-                        }
-                        nextTable = source.nextTable();
-                }
-
-                Set<Candidate> candidates = generator.generateCandidates();
-
-                System.out.println("# Candidates");
-                candidates.forEach(cand -> System.out.println(cand));
-
-        }
+    }
 }
---
+++ src/main/java/de/ddm/profiler/Utility.java
@@ -0,0 +1,35 @@
+package de.ddm.profiler;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public abstract class Utility {
+    public static Map<Value, Long> calulateValueCountDelta(List<ValueWithPosition> newValues, List<Value> oldValues){
+        Map<Value, Long> decreasedCounts = oldValues.stream().collect(Collectors.groupingBy(
+                        v -> v, Collectors.counting()));
+        Map<Value, Long> deltaCounts = newValues.stream().collect(Collectors.groupingBy(
+                        v -> v.value, Collectors.counting()));
+
+        for (Map.Entry<Value, Long> entry : decreasedCounts.entrySet()) {
+            deltaCounts.merge(
+                   entry.getKey(),
+                   -entry.getValue(),
+                   (positiveCount, negativeCount) -> positiveCount - negativeCount);
+                    // TODO remove if delta count is 0?
+        }
+
+        return deltaCounts;
+    }
+
+    public static SetChange calculateSetChange(Map<Value, Long> deltaCounts, Map<Value, Long> totalCounts){
+        Set<Value> addedValues = totalCounts.entrySet().stream()
+                .filter(entry -> entry.getValue() == deltaCounts.get(entry.getKey()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+        Set<Value> removedValues = totalCounts.entrySet().stream()
+                .filter(entry -> entry.getValue() == 0)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+        return new SetChange(addedValues, removedValues);
+    }
+}
