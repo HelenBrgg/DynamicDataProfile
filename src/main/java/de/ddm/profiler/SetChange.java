b9ab5f84271168b86bc937ef65c206fe64655fc2
Felix Köpge <felix.koepge@schmidt-embedded-systems.de>
2022-04-16 17:08:41 +0200

    ensure .inserted and .removed in SetChange are distinct

--- src/main/java/de/ddm/profiler/SetChange.java
+++ src/main/java/de/ddm/profiler/SetChange.java
@@ -1,11 +1,17 @@
 package de.ddm.profiler;
 
-import lombok.AllArgsConstructor;
 import java.util.Set;
+import java.util.ArraySet;
 
-@AllArgsConstructor
 public class SetChange {
     public Set<Value> inserted;
     public Set<Value> removed;
 
+    public SetChange(Set<Value> inserted, Set<Value> removed){
+        // we ensure the two sets are distinct (element was added and removed = no change)
+        this.inserted = new ArraySet<>(inserted);
+        this.inserted.removeAll(removed);
+        this.removed = new ArraySet<>(removed);
+        this.removed.removeAll(inserted);
+    }
 }
