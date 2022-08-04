adb3ed891e178e7714d3b194957ab689ab61b1cd
Ragna <ragna@schwalbach.de>
2022-08-04 13:16:49 +0200

    Kommentare fertiggestellt

--- datagen/datagen_neu.py
+++ datagen/datagen_neu.py
@@ -12,6 +12,11 @@ class CSVRowReader:
     """takes a CVS-File and return the rows together with an index as arrays
     """
     def __init__(self, filename):
+        """Constructor method
+
+        :param filename: location of a CSV file
+        :type filename: CSV file
+        """
         self.reader = csv.reader(open(filename, encoding='utf-8'),delimiter=',')
         self._attributes = next(self.reader)
         self.filename = filename
@@ -20,8 +25,8 @@ class CSVRowReader:
     def attributes(self):
         """returns the attributes (first line of File) of the dataset.
 
-        :return: Index '$' with attribute names
-        :rtype: Array
+        :return: Index '$' + attribute names
+        :rtype: Array[String]
         """
         return ['$'] + self._attributes
 
@@ -30,7 +35,7 @@ class CSVRowReader:
         If this is not possible (e.g. no more row left) the iteration stops and returns None
 
         :return: as long as possible: Index + row of reader. If no longer possible: None
-        :rtype: Array
+        :rtype: Array[String]
         """
         try:
             self.index += 1
@@ -46,17 +51,39 @@ class CSVRowReader:
         next(self.reader)
 
 
-# takes a read file and repeats the rows until the specified number of maximum rows is reached
 class RowRepeater:
+    """takes the reader (inner) and repeats the rows until a specified number of maximum rows is reached
+    """
     def __init__(self, max_row, inner):
+        """Constructor method
+
+        :param max_row: number of row to returned
+        :type max_row: _type_
+        :param inner: RowRepeater is a inner-funtion of the CSVRowReader
+        """
         self.max_row = max_row
         self.inner = inner
         self.remaining_rows = max_row
     
     def attributes(self):
+        """returns the attribute of the passed reader
+
+        :return: calls the attribute function of the passed generator
+        :rtype: Array[String]
+        """
         return self.inner.attributes()
 
     def nextRow(self):
+        """If remaining_rows is not equal to 0: 
+            call the nextRow def of the given reader and decrease remaining_rows by 1
+                if it returns "None" than it call the reset def of the given reader 
+                call the readers nextRow def again
+            the index gets adjusted and the row gets returned
+        Else retun None
+
+        :return: None or Index + row of reader
+        :rtype: None or Array[String]
+        """
         if self.remaining_rows != 0:
             next_row = self.inner.nextRow()
             self.remaining_rows -= 1
@@ -69,21 +96,43 @@ class RowRepeater:
             return None
     
     def reset(self):
+        """resets the RowRepeater by calling for the reset def of the given reader 
+        and resets the remeining_rows to the max_row again
+        """
         self.remaining_rows = self.max_row
         self.inner.reset()
 
 
-# deleats a row of a given row_generator with a specified delete chance
 class RowDeleter:
+    """deleats a row of a given row_generator with a specified delete chance
+    """
     def __init__(self, delete_chance, row_generator):
+        """Constructor method
+
+        :param delete_chance: a number between 0 and 1 
+        :type delete_chance: float
+        :param row_generator: can be eather a CSVRowReader or a RowRepeater
+        """
         self.delete_chance = delete_chance
         self.row_generator = row_generator
         self.max_index = 0
 
     def attributes(self):
+        """returns the attribute of the passed row_generator
+
+        :return: calls the attribute function of the passed generator
+        :rtype: Array[String]
+        """
         return self.row_generator.attributes()
 
     def nextRow(self):
+        """Chooses randomly a number between 0 and 1. 
+        If this number ist smaller than the given delete chance a randomly chosen row gets deleted.
+        Else a new row is returned.
+
+        :return: Either an "old" Index and an empty String for each attribute or a completely new row
+        :rtype: Array[String]
+        """
         if random.uniform(0, 1) < self.delete_chance:
             return [str(random.randint(0, self.max_index))] + ([''] * (len(self.attributes())-1))
         else:
@@ -93,17 +142,35 @@ class RowDeleter:
             return row
     
     def reset(self):
+        """resets the deleter and calls the reset def from the given row_generator.
+        """
         self.max_index = 0
         self.row_generator.reset()
 
 
-# takes a row_generator and a generates a batch with a specified maximum size
 class Batcher:
+    """takes a row_generator (an instance of upper classes) and generates a batch with a specified maximum size
+    """
     def __init__(self, row_generator, max_batch_size):
+        """Constructor method
+
+        :param row_generator: can ether be an instance of CSVRowReader, RowRepeater or RowDeleter
+        :param max_batch_size: maximum size of the batch 
+        :type max_batch_size: int
+        """
         self.row_generator = row_generator
         self.max_batch_size = max_batch_size
     
     def nextBatch(self):
+        """calls the attribute def and joined them with commas. 
+        As long as the length of the batch is smaler then the max_batch_size 
+            the nextRow dev of the given row_generator gets called and the individual strings of each row gets joind with a comma
+        At last the the attribute string and the batch gets connected
+
+        :return: A list of rows where the rows are seperated by a new line 
+        and the entries of each row sepersted by comma and 
+        :rtype: String
+        """
         batch = ''
         attributes = self.row_generator.attributes()
         attr_string = ','.join(attributes)
@@ -118,23 +185,33 @@ class Batcher:
         return attr_string + batch
 
 
-# takes the location of a CSV-file, number of row and a delete chance, 
-# create a row_generatior, 
-# give it to the Batcher and creates a batch
 class CSVReadIn:
+    """takes the location of a CSV-file and number of rows, 
+    creates a reader, then a repeater, then a deleter and than give it to the Batcher 
+    creates a batch that can be submitted to the system
+    """
     def __init__(self):
+        """Constructor method
+        """
         self.goal = sys.argv[1] #wo hin soll die CSV
         self.adress_csv = sys.argv[2] #Pfad wo die Datei liegt
         self.number_of_rows = int(sys.argv[3])
         self.delete_chance = 0.1
     
     def openRowGenerator(self):
+        """creates a instanc of RowDeleter (deleter) by creating a reader (instanc of CSVRowReader) and than a 
+        repeater (instance of RowRepeater).
+
+        :return: a deleter with the given deleat_chance and a repeater
+        """
         reader = CSVRowReader(self.adress_csv)
         repeater = RowRepeater(self.number_of_rows, reader)
         deleter = RowDeleter(self.delete_chance, repeater)
         return deleter
     
     def run(self):
+        """Calls the Batcher class with a max_batch_size of 1000
+        """
         batcher = Batcher(self.openRowGenerator(), 1000)
         batch = batcher.nextBatch()
         while batch is not None:
@@ -169,8 +246,8 @@ class CSVReadIn:
 
 
 
-#reader1=CSVRowReader('/Users/ilove/OneDrive/Uni/Master - Philipps Uni/1. Semester/Projektarbeit/Projektarbeit/DynamicDataProfile-1/datagen/adressen.csv')
-#reader=RowRepeater(12, reader1)
+# reader1=CSVRowReader('/Users/ilove/OneDrive/Uni/Master - Philipps Uni/1. Semester/Projektarbeit/Projektarbeit/DynamicDataProfile-1/datagen/adressen.csv')
+# reader=RowRepeater(12, reader1)
 
 # row = reader.nextRow()
 # attributes = reader.attributes()
@@ -190,7 +267,7 @@ class CSVReadIn:
 # print(batcher.nextBatch())
 # print(batcher.nextBatch())
 
-readin = CSVReadIn()
-readin.run()
+# readin = CSVReadIn()
+# readin.run()
 
 # print(sys.argv)
